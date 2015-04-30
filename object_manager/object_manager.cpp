07b01ee4dc9a07e9f16016dce033cbf52e4ab552
Dudkin <dudkin@62kaf.vka>
2015-04-30 12:24:25 +0400

    30 april GITе

--- object_manager/object_manager.cpp
+++ object_manager/object_manager.cpp
@@ -4183,78 +4183,78 @@ return;
 //=============== Автоперевод систем координат ====================
 void Objectmanager::WGS_to_other()
 {
-    //================MessageBox===============================
-    QMessageBox msgBox;
-    msgBox.setWindowTitle("Сообщение");
-    msgBox.setText("You need a map to translate the object coordinates.\nDo you want to open the map?");
-    msgBox.setStandardButtons(QMessageBox::Yes | QMessageBox::No);
-    msgBox.setButtonText(QMessageBox::Yes, "Да");
-    msgBox.setButtonText(QMessageBox::No, "Нет");
+//    //================MessageBox===============================
+//    QMessageBox msgBox;
+//    msgBox.setWindowTitle("Сообщение");
+//    msgBox.setText("You need a map to translate the object coordinates.\nDo you want to open the map?");
+//    msgBox.setStandardButtons(QMessageBox::Yes | QMessageBox::No);
+//    msgBox.setButtonText(QMessageBox::Yes, "Да");
+//    msgBox.setButtonText(QMessageBox::No, "Нет");
 
-     switch (msgBox.exec()) {
-     case QMessageBox::Yes:
-         // yes was clicked
-         break;
-     case QMessageBox::No:
-         return;
-         break;
-     default:
-         return;
-         break;
-     }
-  //==============================================================
+//     switch (msgBox.exec()) {
+//     case QMessageBox::Yes:
+//         // yes was clicked
+//         break;
+//     case QMessageBox::No:
+//         return;
+//         break;
+//     default:
+//         return;
+//         break;
+//     }
+//  //==============================================================
 
-    QString File = QFileDialog::getOpenFileName(this, QString::null, QString::null,"Maps (*.map)" );
-    if (File.isEmpty()) return;//если карта не выбрана
+//    QString File = QFileDialog::getOpenFileName(this, QString::null, QString::null,"Maps (*.map)" );
+//    if (File.isEmpty()) return;//если карта не выбрана
 
-    MyMapAccess *map = new MyMapAccess();
-    hmap = 0;
-    hmap = map->mapOpen(File.toLocal8Bit().data(),0);
-    if(hmap == 0) return;
+//    MyMapAccess *map = new MyMapAccess();
+//    hmap = 0;
+//    hmap = map->mapOpen(File.toLocal8Bit().data(),0);
+//    if(hmap == 0) return;
 
-    if(map->mapIsGeoSupported(hmap))
-    {
-        GEODEGREE N, E;
-        double N_rad, E_rad, H;
+//    if(map->mapIsGeoSupported(hmap))
+//    {
+//        GEODEGREE N, E;
+//        double N_rad, E_rad, H;
 
-        N.Degree = e1->text().toLong();
-        N.Minute = e2->text().toLong();
-        N.Second = e3->text().toFloat();
+//        N.Degree = e1->text().toLong();
+//        N.Minute = e2->text().toLong();
+//        N.Second = e3->text().toFloat();
 
-        map->mapDegreeToRadian(&N, &N_rad);
+//        map->mapDegreeToRadian(&N, &N_rad);
 
-        E.Degree = e4->text().toLong();
-        E.Minute = e5->text().toLong();
-        E.Second = e6->text().toFloat();
+//        E.Degree = e4->text().toLong();
+//        E.Minute = e5->text().toLong();
+//        E.Second = e6->text().toFloat();
 
-        map->mapDegreeToRadian(&E, &E_rad);
+//        map->mapDegreeToRadian(&E, &E_rad);
 
-        //----- Перезаписываем введенные координаты в поля Edit для WGS-84------------
-        e1->setText(QString::number(N.Degree));
-        e2->setText(QString::number(N.Minute));
-        e3->setText(QString::number(N.Second,'f',2));
-        e4->setText(QString::number(E.Degree));
-        e5->setText(QString::number(E.Minute));
-        e6->setText(QString::number(E.Second,'f',2));
-        //-----------------
-        map->mapGeoWGS84ToPlane3D(hmap,&N_rad,&E_rad,&H);
+//        //----- Перезаписываем введенные координаты в поля Edit для WGS-84------------
+//        e1->setText(QString::number(N.Degree));
+//        e2->setText(QString::number(N.Minute));
+//        e3->setText(QString::number(N.Second,'f',2));
+//        e4->setText(QString::number(E.Degree));
+//        e5->setText(QString::number(E.Minute));
+//        e6->setText(QString::number(E.Second,'f',2));
+//        //-----------------
+//        map->mapGeoWGS84ToPlane3D(hmap,&N_rad,&E_rad,&H);
 
-        e15->setText(QString::number(N_rad,'f',2));
-        e16->setText(QString::number(E_rad,'f',2));
+//        e15->setText(QString::number(N_rad,'f',2));
+//        e16->setText(QString::number(E_rad,'f',2));
 
-        map->mapPlaneToGeo423D(hmap,&N_rad,&E_rad,&H);
-        map->mapRadianToDegree(&N_rad, &N);
-        map->mapRadianToDegree(&E_rad, &E);
+//        map->mapPlaneToGeo423D(hmap,&N_rad,&E_rad,&H);
+//        map->mapRadianToDegree(&N_rad, &N);
+//        map->mapRadianToDegree(&E_rad, &E);
 
 
-    }
+//    }
 
-    if(hmap)
-    {
-        map->mapCloseData(hmap);
-    }
+//    if(hmap)
+//    {
+//        map->mapCloseData(hmap);
+//    }
 
-    add_coord->raise();
+//    add_coord->raise();
 }
 
 void Objectmanager::Plain_to_other()
