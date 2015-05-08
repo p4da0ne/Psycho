09fa7efe749606b830eca7da0c9e7e03e1e67aa4
Dudkin <dudkin@62kaf.vka>
2015-05-08 14:10:07 +0400

    change view and chutka mapview

--- mapview/mapview.cpp
+++ mapview/mapview.cpp
@@ -948,7 +948,7 @@ void MapView::showCheckedObjects()
 		//показать формирования
 		closeSitByName(formationsSitName);
 		HSITE formationsSite = openMapSit(formationsSitName,rscPath);
-		QList<SignData*> formationsSigns = model->getFormations();
+		QList<SignData*> formationsSigns = model->getFormations(mapwin->hMap,x1,y1,x2,y2);
 		createSitObjects(formationsSite, formationsSigns);
 	}
 	else
@@ -962,7 +962,7 @@ void MapView::showCheckedObjects()
 		//показать особые условия
 		closeSitByName(conditionsSitName);
 		HSITE conditionsSite = openMapSit(conditionsSitName,rscPath);
-		QList<SignData*> specialConditionsSigns = model->getSpecialConditions();
+		QList<SignData*> specialConditionsSigns = model->getSpecialConditions(mapwin->hMap,x1,y1,x2,y2);
 		createSitObjects(conditionsSite, specialConditionsSigns);
 	}
 	else
