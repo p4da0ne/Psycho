46509b8653662b91a366ce72301c740b044583a9
Âëàäèìèð Ëîõâèöêèé <lva@63kaf.vka>
2015-05-14 09:27:26 +0400

    Added regions painting on map from DB

--- mapview/mapscroll.cpp
+++ mapview/mapscroll.cpp
@@ -955,6 +955,8 @@ long int	MapScroll::createObjectTest(long int hSit,  QList<Coord*> *coordinates,
 	info = map->mapCreateSiteObject(hMap,hSit);
 	objectTopScale(info);
 
+	map->mapRegisterObjectByKey(info, rscKey);
+	
 	if(semantics)
 	{
 		QMap<long int,QString>::iterator it = semantics->begin(); 
@@ -966,7 +968,7 @@ long int	MapScroll::createObjectTest(long int hSit,  QList<Coord*> *coordinates,
 		}
 	}
 
-	map->mapRegisterObjectByKey(info, rscKey);
+
 
 	//-- Добавление метрики объекта из списка координат ----
 	for(int i=0;i<coordinates->count();i++)
@@ -986,8 +988,6 @@ long int	MapScroll::createObjectTest(long int hSit,  QList<Coord*> *coordinates,
 	return commitFlag;
 }
 
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
