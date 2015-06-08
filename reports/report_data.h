e33a9e5b71db1f5db53eafcc1ba28ad9bf2af454
Lochvitskiy Vladimir <vovan296@mail.ru>
2015-06-08 22:49:16 +0300

    qwerty

--- reports/report_data.h
+++ reports/report_data.h
@@ -54,7 +54,7 @@ public:
 	QString getEducOrganizationsInfo(int idPersones, int idEducType);
 	QString getScienceRankInfo(int idPersones);
 	QString getFamilyAddress(int idPersones);
-	QString getMotherFatherData(int idPersones);
+	QString getFamilyData(int idPersones, bool isParent);
 
 //	QMultiMap<QString,QString> get_obj_v(int id_object = 0);
    
